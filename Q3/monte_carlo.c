02455dc0541eec2dbd670d23a705ff2c7aa8274e
Leonardo Souza <leonardostsouza@gmail.com>
2016-07-05 19:22:24 -0300

    Implement parallel execution with OpenMP (Q3 - Monte Carlo)

--- Q3/monte_carlo.c
+++ Q3/monte_carlo.c
@@ -5,7 +5,9 @@
 int main(int argc, char* argv[])
 {
 	double x, y, z, pi;
-	unsigned int itt, niter, count;
+	unsigned int itt, niter, countM;
+
+	//printf("rdtsc: %lu\n",(unsigned long)rdtsc());
 
 	// check parameter
 	if (argc != 2)
@@ -16,30 +18,27 @@ int main(int argc, char* argv[])
 	{
 		// initialize variables
 		niter = atoi(argv[1]);
-		count = 0;
+		countM = 0;
 		srand48(rdtsc());
 
-		#pragma omp parallel shared(count, niter) private(x, y, itt) num_threads(8)
+		#pragma omp parallel for shared(niter) private(x, y, itt) reduction(+:countM) num_threads(8)
+		for(itt = 0; itt < niter; itt++)
 		{
-			#pragma omp for
-			for(itt = 0; itt < niter; itt++)
-			{
-				// generate point
-				x = drand48() - 0.5;
-				y = drand48() - 0.5;
+			// generate point
+			x = drand48() - 0.5;
+			y = drand48() - 0.5;
 
-				// check if point is inside circle
-				if (((x*x) + (y*y)) <= (RADIUS*RADIUS))//isInsideCircle(x, y) == 1)
-				{
-					count ++;
-				}
-		}
+			// check if point is inside circle
+			if (((x*x) + (y*y)) <= (RADIUS*RADIUS))//isInsideCircle(x, y) == 1)
+			{
+				countM++;
+			}
 		}
 
 		// calculate PI
-		pi = (count/(double)niter) * 4;
+		pi = (countM/(double)niter) * 4;
 
-		printf("N = %d, M = %d, PI = %.5f\n", niter, count, pi);		
+		printf("N = %u, M = %u, PI = %.5f\n", niter, countM, pi);		
 	}
 	return 0;
 }
--- Q3/utils.h
+++ Q3/utils.h
@@ -1,10 +1,11 @@
-#include <math.h>
+#include <stdint.h>
 
 // useful definitions
 #define RADIUS	0.5
 
 // return number of cicles since start
-int rdtsc() 
-{ 
-    __asm__ __volatile__ ("rdtsc"); 
+uint64_t rdtsc(){
+    unsigned int lo,hi;
+    __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
+    return ((uint64_t)hi << 32) | lo;
 }
