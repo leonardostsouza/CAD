d04ddf4fe22189b36d3bbed471d744db76b24160
Leonardo Souza <leonardostsouza@gmail.com>
2016-07-08 18:45:47 -0300

    optimizations on monte_carlo.c

--- Q3/monte_carlo.c
+++ Q3/monte_carlo.c
@@ -4,43 +4,48 @@
 
 int main(int argc, char* argv[])
 {
-	double x, y, z, pi;
-	unsigned int itt, N, M, qtt_threads;
-
-	//printf("rdtsc: %lu\n",(unsigned long)rdtsc());
+	double pi;
+	unsigned int qtt_threads;
+	unsigned long N, M;
 
 	// check parameter
 	if (argc != 3)
 	{
-		printf("Usage: \"mcpi <N> <M>\" , where N is the # of random points to be generated and M is the number of threads\n");
+		printf("Usage: \"mcpi <N> <K>\" , where N is the # of random points to be generated and K is the number of threads\n");
 	}
 	else
 	{
 		// initialize variables
-		N = atoi(argv[1]);
+		N = strtoul(argv[1], NULL, 10);//atoi(argv[1]);
 		qtt_threads = atoi(argv[2]);
 		M = 0;
-		srand48(rdtsc());
+		srand48(0);//rdtsc());
 
-		#pragma omp parallel for shared(N) private(x, y, itt) reduction(+:M) num_threads(qtt_threads)
-		for(itt = 0; itt < N; itt++)
+		#pragma omp parallel firstprivate(N) reduction(+:M) num_threads(qtt_threads)
 		{
-			// generate point
-			x = drand48() - 0.5;
-			y = drand48() - 0.5;
-
-			// check if point is inside circle
-			z = (x*x) + (y*y);
-			if (z <= RADIUS_2)
+			unsigned int itt = 0;
+			unsigned int tempM = 0;
+			#pragma omp for
+			for(itt = 0; itt < N; itt++)
 			{
-				M++;
+				// generate point
+				double x = drand48() - 0.5;
+				double y = drand48() - 0.5;
+
+				// check if point is inside circle
+				double z = (x*x) + (y*y);
+				if (z <= RADIUS_2)
+				{
+					tempM++;
+				}
 			}
+			M += tempM;
 		}
 
 		// calculate PI
 		pi = (M/(double)N) * 4;
 
-		printf("N = %u, M = %u, PI = %.5f\n", N, M, pi);		
+		printf("N = %lu, M = %lu, PI = %.5f\n", N, M, pi);		
 	}
 	return 0;
 }
