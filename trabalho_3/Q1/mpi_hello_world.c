793e937caf8e730127d4a6434feb28ea5f35f9f0
Leonardo Souza <leonardostsouza@gmail.com>
2016-07-14 18:00:13 -0300

    Commit inicial: trabalho 3

---
+++ trabalho_3/Q1/mpi_hello_world.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char** argv) {
+    MPI_Init(NULL, NULL);
+    
+    /////////////// INITIALIZATION ///////////////
+    // get world size
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // get proccess rank
+    int proc_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
+    ///////////// END INITIALIZATION /////////////
+
+    printf("Proccess rank %d sends \"Hello\"!"
+    	" There are %d proccesses running\n", 
+    	proc_rank, world_size);
+    MPI_Finalize();
+}
