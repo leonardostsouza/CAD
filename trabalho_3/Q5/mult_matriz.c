a3cc5de51ac460dfc705b720ca1ed0137e4e04a5
patykov <patriciakovaleski@gmail.com>
2016-07-24 00:49:47 -0300

    Voltando a multiplicacao ao normal

--- trabalho_3/Q5/mult_matriz.c
+++ trabalho_3/Q5/mult_matriz.c
@@ -2,9 +2,18 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-#define SIZE 4
-#define MALLOC
+#define SIZE 500
+//#define MALLOC
 
+void printMatrix(double** M, int size){
+    int i, j;
+    for (i = 0; i < size; i++){
+        printf("\n");
+        for (j = 0; j < size; j++){
+            printf("%6.2f\t", M[i][j]);
+        }
+    }
+}
 
 int main(int argc, char** argv) {
     int send_partner, recv_partner, rows, world_size, proc_rank;
@@ -12,17 +21,9 @@ int main(int argc, char** argv) {
     MPI_Status status;
 
     #ifdef MALLOC
-        double *A, *B, *C;
+        double **A, **B, **C;
     #else
         double A[SIZE][SIZE], B[SIZE][SIZE], C[SIZE][SIZE];
-    #endif
-
-    #ifdef MALLOC
-        // Criar matrizes
-        A = (double *) malloc((SIZE*SIZE)*sizeof(double));
-        B = (double *) malloc((SIZE*SIZE)*sizeof(double));
-        C = (double *) malloc((SIZE*SIZE)*sizeof(double));
-
     #endif
     
     //MPI_Request reqSendA[world_size], reqSendB[world_size], reqRecv[world_size];
@@ -35,6 +36,18 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
     ///////////// END INITIALIZATION /////////////
 
+    #ifdef MALLOC
+        // Criar matrizes
+        A = (double**) malloc(sizeof(*A) * SIZE);
+        B = (double**) malloc(sizeof(*B) * SIZE);
+        C = (double**) malloc(sizeof(*C) * SIZE);
+
+        for (i = 0; i<SIZE; i++){
+            A[i] = (double*) malloc(sizeof(*A[i]) * SIZE);
+            B[i] = (double*) malloc(sizeof(*A[i]) * SIZE);
+            C[i] = (double*) malloc(sizeof(*A[i]) * SIZE);
+        }
+    #endif
 
     /////////////// MASTER ///////////////
     if (proc_rank == 0){
@@ -44,9 +57,11 @@ int main(int argc, char** argv) {
         proc_rank, world_size);
 
         // Fill matrices
-        for (i = 0; i < (SIZE*SIZE); i++){
-            A[i] = 1;
-            B[i] = 1;
+        for (i = 0; i < SIZE; i++){
+            for (j = 0; j < SIZE; j++) {
+                A[i][j] = 1;
+                B[i][j] = 1;
+            }
         }
 
 
@@ -54,35 +69,33 @@ int main(int argc, char** argv) {
         num_rows = SIZE/(world_size-1);
         rest = SIZE%(world_size-1);
         offset = 0;
-        int off = 0;
         for (send_partner = 1; send_partner < world_size; send_partner++){
             rows = (send_partner <= rest) ? num_rows+1 : num_rows;
-            printf("MASTER sending %d rows to process %d (%d to %d)\n", rows, send_partner, off, off+rows-1);
+            printf("MASTER sending %d rows to process %d (%d to %d)\n", rows, send_partner, offset, offset+rows-1);
             MPI_Send(&offset, 1, MPI_INT, send_partner, 0, MPI_COMM_WORLD);
             MPI_Send(&rows, 1, MPI_INT, send_partner, 0, MPI_COMM_WORLD);
-            MPI_Send(&A[offset], rows*SIZE, MPI_DOUBLE, send_partner, 0, MPI_COMM_WORLD);
+            MPI_Send(&A[offset][0], rows*SIZE, MPI_DOUBLE, send_partner, 0, MPI_COMM_WORLD);
             MPI_Send(&B, SIZE*SIZE, MPI_DOUBLE, send_partner, 0, MPI_COMM_WORLD);
-            offset = offset + (rows*SIZE);
-            off = off+1;
+            offset = offset + rows;
         }
 
         // Receive results
         for (recv_partner=1; recv_partner < world_size; recv_partner++) {
             MPI_Recv(&offset, 1, MPI_INT, recv_partner, 0, MPI_COMM_WORLD, &status);
             MPI_Recv(&rows, 1, MPI_INT, recv_partner, 0, MPI_COMM_WORLD, &status);
-            MPI_Recv(&C[offset], rows*SIZE, MPI_DOUBLE, recv_partner, 0, MPI_COMM_WORLD, &status);
+            MPI_Recv(&C[offset][0], rows*SIZE, MPI_DOUBLE, recv_partner, 0, MPI_COMM_WORLD, &status);
             printf("Proccess %d has finnished\n",recv_partner);
         }
         printf("Multiplication DONE! Printing results:\n");
 
         // Print matrix C
-        for (i = 0; i < (SIZE*SIZE); i++){
-            if(!i%SIZE){
-                printf("\n");
-            }
-            printf("%6.2f\t", C[i]);
-        }
-        printf("\n");
+        // for (i = 0; i < SIZE; i++){
+        //     printf("\n");
+        //     for (j = 0; j < SIZE; j++){
+        //         printf("%6.2f\t", C[i][j]);
+        //     }
+        // }
+        // printf("\n");
     }
 
     /////////////// WORKER ///////////////
@@ -90,25 +103,16 @@ int main(int argc, char** argv) {
         // Receive Matrices
         MPI_Recv(&offset, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
         MPI_Recv(&rows, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-        MPI_Recv(&A[offset], rows*SIZE, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
+        MPI_Recv(&A, rows*SIZE, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
         MPI_Recv(&B, SIZE*SIZE, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
 
         // Compute A*B
-        // for (k=0; k<SIZE; k++) {
-        //     for (i=0; i<rows; i++) {
-        //         C[i][k] = 0.0;
-        //         for (j=0; j<SIZE; j++){
-        //             C[i][k] += A[i][j] * B[j][k];
-        //         }
-        //     }
-        // }
-
-        for (i = 0; i < rows; i++) {
-            for (j = 0; j < SIZE; j++) {
-                double sum = 0.0;
-                for (k = 0; k < SIZE; k++)
-                    sum = sum + A[i * SIZE + k] * B[k * SIZE + j];
-                C[i * SIZE + j] = sum;
+        for (k=0; k<SIZE; k++) {
+            for (i=0; i<rows; i++) {
+                C[i][k] = 0.0;
+                for (j=0; j<SIZE; j++){
+                    C[i][k] += A[i][j] * B[j][k];
+                }
             }
         }
 
@@ -119,11 +123,6 @@ int main(int argc, char** argv) {
     }
 
     #ifdef MALLOC
-    	for (i = 0; i < SIZE; i++){
-    		free(A[i]);
-    		free(B[i]);
-    		free(C[i]);
-    	}
         free(A);
         free(B);
         free(C);
